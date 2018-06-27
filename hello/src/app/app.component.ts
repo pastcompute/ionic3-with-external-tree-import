b8f84c39d9f5b51b1f4d2333235e87f3e381d78b
Andrew McDonnell <bugs@andrewmcdonnell.net>
2018-06-27 12:46:14 +0930

    browser run debug works

--- hello/src/app/app.component.ts
+++ hello/src/app/app.component.ts
@@ -4,18 +4,21 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+import { HelloClass } from '../../../shared';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, hello: HelloClass) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
+      // splashScreen.hide();
     });
   }
 }
--- shared/index.ts
+++ shared/index.ts
@@ -1,2 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
 export const HELLO = 'world';
+@Injectable()
+export class HelloClass {
+    constructor(platform: Platform, splashScreen: SplashScreen) {
+        console.log(1);
+        platform.ready().then(() => {
+            splashScreen.hide();
+            console.log(2);
+        });
+    }
+};
 
+// @NgModule({
+
+// })
+// export class HelloModule;
